cc77bd729508cf5762c3efe6b35861f906e1197a
ParmeshwarMall <142672221+ParmeshwarMall@users.noreply.github.com>
2024-10-18 20:22:13 +0530

    index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://bharat-bank.netlify.app/",
     credentials: true,
   })
 );
